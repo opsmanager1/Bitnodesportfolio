6cd0fba8626314d9f375d0a5b5e2d434b0acbf0e
Serhii <135651866+opsmanager1@users.noreply.github.com>
2024-08-13 16:20:54 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ const home = () => {
             <MyExpertise />
             <Recommendations />
             <ClientReviews />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
