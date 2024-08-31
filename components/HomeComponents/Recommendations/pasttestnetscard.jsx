855e99d6fd24fcdb3c8d4c3c1c05306f23d08bf0
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2024-08-31 18:27:16 +0300

    Update pasttestnetscard.jsx

--- components/HomeComponents/Recommendations/pasttestnetscard.jsx
+++ components/HomeComponents/Recommendations/pasttestnetscard.jsx
@@ -3,7 +3,7 @@ import CardLayout2 from '../../Common/CardLayout2'
 
 const pasttestnetscard = ({ data }) => {
     return (
-        <CardLayout>
+        <CardLayout2>
             <div className="p-8 h-full relative card_stylings transition">
                 <img src={data?.image} className="absolute z-10 right-10 -top-5 border-Green w-16 h-16  border-[3px] rounded-full m-0" />
                 <div className=" text-Snow underline italic">
@@ -12,7 +12,7 @@ const pasttestnetscard = ({ data }) => {
                 <div className="text-xs text-LightGray italic mt-1">{data?.designation}</div>
                 <div className="text-sm mt-2 text-LightGray font-normal ">{data?.view}</div>
             </div>
-        </CardLayout>
+        </CardLayout2>
     )
 }
 
