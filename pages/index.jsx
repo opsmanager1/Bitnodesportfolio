aba000d996ce0bfc0b5e26acceb38be1c22035d6
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2024-08-31 18:20:17 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Footer from '../components/Footer';
 import Banner from '../components/HomeComponents/Banner';
 import MyExpertise from '../components/HomeComponents/Expertise/MyExpertise';
 import Recommendations from '../components/HomeComponents/Recommendations/Recommendations';
-import pasttestnets from '../components/HomeComponents/pasttestnets';
+import pasttestnets from '../components/HomeComponents/Recommendations/pasttestnets';
 import ClientReviews from '../components/HomeComponents/ClientReviews/ClientReviews';
 const home = () => {
     return (
