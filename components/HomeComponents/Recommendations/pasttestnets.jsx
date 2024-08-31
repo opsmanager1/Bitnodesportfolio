d725ae63353712ac542368f0792ea3109868146f
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2024-08-31 18:09:43 +0300

    Update pasttestnets.jsx

--- components/HomeComponents/Recommendations/pasttestnets.jsx
+++ components/HomeComponents/Recommendations/pasttestnets.jsx
@@ -1,1 +1,34 @@
+import pasttestnetscard from "./pasttestnetscard"
+import axios from "axios";
+import { useQuery } from "react-query";
+import ParagraphSkeleton2 from "../../Common/ParagraphSkeleton2";
 
+
+const pasttestnets = () => {
+    const { isLoading, error, data } = useQuery('pasttestnets', () =>
+        axios.get('api/pasttestnets')
+            .then(({ data }) => data)
+            .catch(error => console.error('Error fetching testimonials:', error)))
+
+
+    return (
+        <>
+            <div className="px-2 md:px-8 py-4 text-lg font-bold text-Snow">Ended</div>
+            <div className="grid w-full h-full mt-5 justify-items-start grid-flow-row md:grid-cols-2 grid-rows-auto gap-x-4 gap-y-4 px-2 md:px-8 pb-8">
+
+                {isLoading ?
+                    [1, 2, 3, 4].map(() => (
+                        <ParagraphSkeleton2 className={"p-8 h-full w-full relative"} />
+                    ))
+                    :
+                    data?.map((data, key) => (
+                        <pasttestnetscard key={key} data={data} />
+                    ))
+                }
+
+            </div>
+        </>
+    )
+}
+
+export default pasttestnets
