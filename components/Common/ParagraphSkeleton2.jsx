3f60cd00ba0e28a819bbac6e4f85f4e7e285f349
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2024-08-31 18:12:39 +0300

    Update ParagraphSkeleton2.jsx

--- components/Common/ParagraphSkeleton2.jsx
+++ components/Common/ParagraphSkeleton2.jsx
@@ -5,7 +5,7 @@ import CardLayout2 from './CardLayout2'
 const ParagraphSkeleton2 = ({ className }) => {
     return (
 
-        <CardLayout>
+        <CardLayout2>
             <div role="status" class={`${className} max-w-full card_stylings animate-pulse bg-EveningBlack self-center`}>
                 <div class="h-2.5 rounded-full bg-Green/20 w-48 mb-4"></div>
                 <div class="h-2 rounded-full bg-Green/20 max-w-[365px] mb-2.5"></div>
@@ -15,7 +15,7 @@ const ParagraphSkeleton2 = ({ className }) => {
                 <div class="h-2 rounded-full bg-Green/20 max-w-[360px]"></div>
                 <span class="sr-only">Loading...</span>
             </div>
-        </CardLayout>
+        </CardLayout2>
 
     )
 }
