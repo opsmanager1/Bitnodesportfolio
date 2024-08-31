7f678af7b8cfefd4b9527d4edc0746c98b239582
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2024-08-31 22:09:46 +0300

    Update Languages.jsx

--- components/Common/Intro/Languages.jsx
+++ components/Common/Intro/Languages.jsx
@@ -16,20 +16,20 @@ const Languages = () => {
         }, 30);
  {/* middle components */}
         return () => clearInterval(timer);
-    }, [urdu, english])
+    }, [Uptime, english])
     return (
         <div className="flex flex-col space-y-1 pt-6">
             <div className="flex flex-col gap-y-4">
-                <span className='text-Snow text-xs font-bold'>Languages</span>
+                <span className='text-Snow text-xs font-bold'>Nodes performance</span>
                 <div className="flex flex-row items-center justify-center space-x-6">
                     <div className="flex flex-col items-center justify-center gap-y-2">
-                        <Progress strokeColor="#1fdf64" type="circle" percent={urdu} size={75} />
-                        <span className='text-xs font-bold text-Snow'>Urdu</span>
+                        <Progress strokeColor="#1fdf64" type="circle" percent={Uptime} size={75} />
+                        <span className='text-xs font-bold text-Snow'>Uptime</span>
                     </div>
-                    <div className="flex flex-col items-center justify-center gap-y-2">
+                     {/* <div className="flex flex-col items-center justify-center gap-y-2">
                         <Progress strokeColor="#1fdf64" type="circle" percent={english} size={75} />
                         <span className='text-xs font-bold text-Snow'>English</span>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
