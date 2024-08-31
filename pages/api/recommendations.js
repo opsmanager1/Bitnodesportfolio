4fca5e750708893504e15f7890c1668d50e5d115
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2024-08-31 12:47:17 +0300

    Update recommendations.js

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -4,9 +4,11 @@ const recommendationCard = [
     id: 0,
     name: 'Empeira',
     image: "images/Empeira.png",
-    designation: 'React JS | NEXT JS | Node | GraphQL',
-    view: "Hey everyone! I wanted to take a moment to recommend Osama Javaid for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Osama is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
-    linkednURL: "https://www.linkedin.com/in/shahmir-minhas/"
+    designation: 'Cosmos SDK',
+    view: "
+End-to-End Verifiable Data Infrastructure (EVDI) Unlock the potential of verifiable data with Empeiria EVDI. Build easily deployable, innovative solutions, seamlessly bridging off-chain and on-chain worlds, empowering organizations to embrace Web3 technology.
+🌟",
+    linkednURL: "https://www.linkedin.com/company/empe-web3/"
   },
   {
     id: 1,
