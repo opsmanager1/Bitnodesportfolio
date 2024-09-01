caf6cc47c25fd362afebdda502919db1f325e1a1
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2024-09-01 15:07:40 +0300

    Update Pasttestnets.js

--- pages/api/Pasttestnets.js
+++ pages/api/Pasttestnets.js
@@ -36,7 +36,7 @@ const Pasttestnetscard = [
    {
     id: 4,
     name: 'Initia',
-    image: "initia.png",
+    image: "images/initia.png",
     designation: 'Modular Cosmos SDK / EVM compatible L1 blockchain',
     view: "In the ever-evolving landscape of the blockchain world, Initia stands as a beacon of innovation and simplicity. We are on a mission to redefine the multi-chain network experience from the ground up. By seamlessly integrating architecture, product, and economic systems, Initia offers a holistic solution that transforms how networks operate, feel, and interact. This is why we call Initia a network for interwoven rollups. ",
     linkednURL: "https://initia.xyz/"
