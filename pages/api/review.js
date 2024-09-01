658cee138085f7fa8229371f3c4875b5150e7fdb
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2024-09-01 15:21:25 +0300

    Update review.js

--- pages/api/review.js
+++ pages/api/review.js
@@ -8,24 +8,24 @@ const review = [
     },
     {
         id: 2,
-        clientName: 'Melissabrown255',
-        clientLocation: 'United States',
-        clientSource: 'Fiverr',
-        clientReview: 'He is very good with communication and experienced website developer, great experience to work with him. '
+        clientName: 'The Role of Polymer as an Interoperability Hub',
+        clientLocation: 'Medium',
+        clientSource: 'Feb 29, 2024',
+        clientReview: 'In the dynamic landscape of blockchain technology, interoperability stands as a crucial element for achieving seamless connectivity between different networks. Within the Ethereum ecosystem, Polymer emerges as a pioneering force, serving as the Interoperability Hub that bridges various Layer 2 solutions through the Inter-Blockchain Communication Protocol... '
     },
     {
         id: 3,
-        clientName: 'Jake Corry',
-        clientLocation: 'Germany',
-        clientSource: 'Fiverr',
-        clientReview: "Osama is extremely knowledgeable and helpful. He knows in and out of web app development . His understanding of different technologies and frameworks in the space is also very impressive. I hope I get the chance to work with him again"
+        clientName: 'Polymer’s Architectural Evolution',
+        clientLocation: 'Medium',
+        clientSource: 'Feb 29, 2024',
+        clientReview: "In the ever-evolving realm of blockchain technology, adaptability and strategic decision-making are paramount. Polymer, a pioneering project helmed by co-founders Bo Du and Peter Kim, exemplifies this ethos through its recent transition from a Cosmos app chain to an Ethereum Layer 2 solution. Central to this transformation are the insights garnered from working with the Cosmos SDK and OP Stack, shedding light on the project’s journey towards interoperability and scalability..."
     },
     {
         id: 4,
-        clientName: 'Blaise Labriola',
-        clientLocation: 'France',
-        clientSource: 'Fiverr',
-        clientReview: 'I have worked with Osama and he is very professional and excellent software  developer. I admire his speed of development and quality of work when given an assignment.'
+        clientName: 'Вперше Initia MoveVM',
+        clientLocation: 'Medium',
+        clientSource: 'Mar 18, 2024',
+        clientReview: 'Вперше Initia MoveVM та модуль x/move привносять мову розумних контрактів Move до CosmosSDK з повноцінною міжблокчейновою взаємодією IBC. Move є однією з трьох віртуальних машин, доступних в переплетених ролапах Initia та Initia L1 Orchestration Layer. Initia MoveVM була створена з особливою увагою до безшовної інтеграції з CosmosSDK. Це означає, що всі активи Move обробляються як рідні активи в межах Cosmos...'
     },
 ]
 export default function handler(req, res) {
