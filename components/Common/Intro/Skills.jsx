e2e31f428fb9b2f3500aae2f58e81d82bd64ff64
Serhii | BITNODES <135651866+opsmanager1@users.noreply.github.com>
2024-09-01 16:32:31 +0300

    Update Skills.jsx

--- components/Common/Intro/Skills.jsx
+++ components/Common/Intro/Skills.jsx
@@ -4,7 +4,7 @@ const Skills = () => {
   return (
     <div className='flex flex-col space-y-1 pt-6'>
       <div className='flex flex-col gap-y-4'>
-        <span className='text-Snow text-xs font-bold bg-gradient-to-bl'>Experties and Competencies</span>
+        <span className='text-Snow text-xs font-bold bg-gradient-to-bl'>Competencies</span>
         <div className='flex flex-col space-y-4'>
           {SKILLS.map((skill, index) => {
             return <LinearBar key={index} title={skill.title} percent={skill.level} bgColor='bg-Green' />;
